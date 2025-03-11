f98325d931e6815ba6ddcabe2cca7c0735cb144c
Jay Vishwakarma <jayvish08@gmail.com>
2025-03-11 17:28:57 +0530

    final

--- utils/schema.js
+++ utils/schema.js
@@ -3,14 +3,14 @@ const Joi = require('joi');
 module.exports.bookSchema = Joi.object({
         title : Joi.string().required(),
         author : Joi.string().required(),
-        publishedYear : Joi.number().required().min(1700),
+        publishedYear : Joi.number().min(1700),
         price : Joi.number().required().min(0),
-        genre : Joi.string().required(),
+        genre : Joi.string()
     }).required();
 
 module.exports.authorSchema = Joi.object({
         name : Joi.string().required(),
-        bio : Joi.string().required(),
+        bio : Joi.string(),
         birthYear : Joi.number().required().min(1700),
         nationality : Joi.string().required(),
         books: Joi.array()
